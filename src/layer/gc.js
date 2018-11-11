05fa0af0c75cb9811f4d4a245623b4825557f65d
foobert <0x002a@gmail.com>
2018-11-11 12:05:46 +0100

    Try to fix geocache position rendering

--- src/layer/gc.js
+++ src/layer/gc.js
@@ -72,13 +72,12 @@ const CanvasLayer = L.GridLayer.extend({
         }
         const position = {
           x:
-            Math.sign(coordinates.lon) *
-            (gc.parsed.lon - coordinates.lon) /
-            Math.abs(coordinates.lon - coordinatesLowerRight.lon) *
+            ((gc.parsed.lon - coordinates.lon) /
+              Math.abs(coordinates.lon - coordinatesLowerRight.lon)) *
             size.x,
           y:
-            (coordinates.lat - gc.parsed.lat) /
-            Math.abs(coordinates.lat - coordinatesLowerRight.lat) *
+            ((coordinates.lat - gc.parsed.lat) /
+              Math.abs(coordinates.lat - coordinatesLowerRight.lat)) *
             size.y
         };
 
